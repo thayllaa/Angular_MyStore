bf4719ee592c01c1ef501b76da52b653d460a953
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-04-19 20:11:46 -0300

    2 files modified, 4 added

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent {
+
+
+}
